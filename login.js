86f94848d0c01f0abd1ed490ee016c99f94d2b49
roinagaki <roinagaki@jotafi.com.ar>
2022-01-04 15:18:19 -0300

    UNION DE JS Y CSS

--- login.js
+++ login.js
@@ -1,29 +1,29 @@
-(function(){
-    $(document).ready(function(){
-        $('.alt-form').click(function(){
-            $('.form-contenido').animate({
-                height: "toggle",
-                opacity: 'toggle'
-            }, 600); 
+// (function(){
+//     $(document).ready(function(){
+//         $('.alt-form').click(function(){
+//             $('.form-contenido').animate({
+//                 height: "toggle",
+//                 opacity: 'toggle'
+//             }, 600); 
 
-        });
-    })
-}())
+//         });
+//     })
+// }())
 
-const email = document.getElementById("correo")
-const pass = document.getElementById("pass")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
+// const email = document.getElementById("correo")
+// const pass = document.getElementById("pass")
+// const form = document.getElementById("form")
+// const parrafo = document.getElementById("warnings")
 
-form.addEventListener("submit",e=>{
-    e.preventDefault()
-    let warnings = ""
-    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
-    if(pass.value.lenght <6){
-        warnings += `La password no es valida`
-    }
-    console.log(regexEmail.text(email.value))
-    if(regexEmail.text(email.value)){
+// form.addEventListener("submit",e=>{
+//     e.preventDefault()
+//     let warnings = ""
+//     let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+//     if(pass.value.lenght <6){
+//         warnings += `La password no es valida`
+//     }
+//     console.log(regexEmail.text(email.value))
+//     if(regexEmail.text(email.value)){
 
-    }
-})
+//     }
+// })
